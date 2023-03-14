f0c60fb91df2d921a4cf14a79113a514fb5a6043
Jones Souza <108988209+jonessouzasilva@users.noreply.github.com>
2023-03-14 11:21:16 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ MenuBtn.addEventListener('click', function(e) {
     document.querySelector('body').classList.toggle('mobile-nav-active');
     this.classList.toggle('fa-xmark')
 })
+
+let typed = new Typed('.auto-input',{
+    strings: ['Web Developer!', 'Front-End!', 'Web Designer!', 'Back-End!', 'Full-Stack!'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+})
